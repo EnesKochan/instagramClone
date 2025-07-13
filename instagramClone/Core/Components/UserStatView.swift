c9f85c721c0c26a7ebce4c524331a9d3cc3983ae
EnesKochan <eneskochann@gmail.com>
2025-07-13 19:44:50 +0300

    Create Profile Page

---
+++ instagramClone/Core/Components/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  instagramClone
+//
+//  Created by Enes Ömer Koçhan on 13.07.2025.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
---
+++ instagramClone/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  instagramClone
+//
+//  Created by Enes Ömer Koçhan on 13.07.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
